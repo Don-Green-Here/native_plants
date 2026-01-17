e4cb1f1e3676ac7c340da6a798999c2be55025fa
Don-Green-Here <don.green.contact@gmail.com>
2026-01-16 16:17:50 -0800

    Scrape USDA on both the characteristics and plant profile page, load it into our 05_usda_characteristics_schema

--- sql/05_usda_characteristics_schema.sql
+++ sql/05_usda_characteristics_schema.sql
@@ -1,5 +1,18 @@
 USE native_plants; 
 
+CREATE TABLE IF NOT EXISTS plant_profile_fetches(
+-- Will take in the general information for the plant
+-- Can source duration (annual, biennial, perrenial)
+symbol VARCHAR(16) NOT NULL,
+profile_url VARCHAR (512) NOT NULL,
+fetched_at DATETIME(6) NOT NULL, 
+fetch_status ENUM ('HAS_DATA', 'NO_DATA', 'ERROR') NOT NULL, 
+error TEXT NULL, 
+PRIMARY KEY (symbol), 
+INDEX idx_status (fetch_status), 
+CONSTRAINT fk_profile_symbol FOREIGN KEY (symbol) REFERENCES canonical_plants(symbol)
+);
+
 CREATE TABLE IF NOT EXISTS plant_characteristics_fetches(
 -- This table will tell us if the plant characteristics exist on the page
 -- So we do not attempt a page that has NO DATA
@@ -17,11 +30,11 @@ CREATE TABLE IF NOT EXISTS plant_characteristics_kv(
 -- Key value pairs for fast searching, this holds the exact values
 id BIGINT UNIQUE AUTO_INCREMENT PRIMARY KEY,
 symbol VARCHAR(16), 
-profile_url VARCHAR(512), 
 section VARCHAR(64), 
 trait_name VARCHAR(64), 
 trait_value TEXT NOT NULL,
 -- Use SHA-256 for text if necessary 
+profile_url VARCHAR(512), 
 fetched_at DATETIME(6),    
 UNIQUE KEY uq_symbol_section_name_value (symbol, section, trait_name, trait_value(255)),
 INDEX idx_symbol (symbol), 
